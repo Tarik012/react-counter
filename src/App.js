dba4112a6b5a36354f8355732e4b13bd35c6928a
TARIK-MAC <administrateur@mbpdetarikmac.lan>
2022-11-02 15:17:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0); // je déclare mon hook
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App-container">
+      <Header />
+      <div className="main">
+        <div className="div-button-haut">
+          <button
+            onClick={() => {
+              // j'appelle l'état du hook dans mon événement click
+              setCounter(counter - 1);
+            }}
+          >
+            {counter === 0 ? "" : "-"}
+          </button>
+          <p>{counter}</p>
+          <button
+            onClick={() => {
+              setCounter(counter + 1);
+            }}
+          >
+            {counter === 10 ? "" : "+"}
+          </button>
+        </div>
+        <button
+          className="btn-reset"
+          onClick={() => {
+            setCounter(0);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Reset
+        </button>
+      </div>
+      <Footer />
     </div>
   );
 }
